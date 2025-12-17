51cbf808ac9bf38c6465c4a5edaa5713f25bf8c9
ZUBAER <zubaer.developer@gmail.com>
2025-12-17 17:05:30 +0600

    feat(setup): Wrap application with AuthProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,19 @@
-import { StrictMode } from 'react'
-import './index.css'
+import { StrictMode } from "react";
+import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from './routes/Routes.jsx';
+import { router } from "./routes/Routes.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Toaster } from 'react-hot-toast';
-import { createRoot } from 'react-dom/client';
+import { Toaster } from "react-hot-toast";
+import { createRoot } from "react-dom/client";
+import AuthProvider from "./context/AuthProvider.jsx";
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-
-  <RouterProvider router={router} />
-  <ToastContainer position="top-right" autoClose={2000} />
-  <Toaster position="top-right" toastOptions={{ duration: 2500 }} />
-
-  </StrictMode>,
-)
-
-
-
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+    <ToastContainer position="top-right" autoClose={2000} />
+    <Toaster position="top-right" toastOptions={{ duration: 2500 }} />
+  </StrictMode>
+);
